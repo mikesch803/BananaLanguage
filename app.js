44cf0d04df98039840e7bff2413a006a73984a1e
mikesch803 <mikesch803@gmail.com>
2020-12-06 21:14:58 +0530

    create btn, txtarea,outputarea, give style

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate = document.querySelector('#translate-btn')
+function clickEventHandler()
+{
+    console.log("clicked")
+}
+btnTranslate.addEventListener("click", clickEventHandler)
