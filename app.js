de9921080b2d08634ead41e1a198e8d13a98db33
mikesch803 <mikesch803@gmail.com>
2020-12-07 19:00:24 +0530

    added query selector and callback function

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var btnTranslate = document.querySelector('#translate-btn')
+var btnTranslate = document.querySelector('#translate-btn');
+var inputText = document.querySelector('#text-input');
+var Output = document.querySelector('#text-output');
+//console.log(Output)
+
 function clickEventHandler()
 {
-    console.log("clicked")
-}
-btnTranslate.addEventListener("click", clickEventHandler)
+    Output.innerText="translated " + inputText.value
+};
+btnTranslate.addEventListener("click", clickEventHandler);
