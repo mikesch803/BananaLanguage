af62816589b4c21ec90240fdad5d2456dadf9d90
mikesch803 <mikesch803@gmail.com>
2020-12-08 00:28:49 +0530

    fetch, errorhandler, mockserver

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var btnTranslate = document.querySelector('#translate-btn');
 var inputText = document.querySelector('#text-input');
 var Output = document.querySelector('#text-output');
-//console.log(Output)
+
+
+//server
+
+var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function errorHandler(){
+    console.log("error occured" ,console.error())
+    alert("something wrong with server")
+}
+
+function translatedUrl(text)
+{
+     return serverURL + "?" + "text=" +text;
+}
 
 function clickEventHandler()
 {
-    Output.innerText="translated " + inputText.value
-};
+    //Output.innerText= inputText.value;
+    var input = inputText.value;
+    fetch(translatedUrl(input))
+        .then (response => response.json())
+        .then (json => console.log(json.contents.translated))
+        .catch(errorHandler)
+    };
+
 btnTranslate.addEventListener("click", clickEventHandler);
